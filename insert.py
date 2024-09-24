e3c7e85b6e506fd97d62e53fee7813d13c4b0e4f
MuhammadYousufMateen9 <167859198+MuhammadYousufMateen9@users.noreply.github.com>
2024-09-24 18:57:32 +0500

    Update insert.py

--- insert.py
+++ insert.py
@@ -1,8 +1,7 @@
-class MethodMania:
-    def ins(lst,data,pos):
-        data=[data]
-        list1=lst[:pos]
-        list2=lst[pos:]
-        list1=list1+data
-        final=list1+list2
-        return final
+def insert(lst,data,pos):
+    data=[data]
+    list1=lst[:pos]
+    list2=lst[pos:]
+    list1=list1+data
+    final=list1+list2
+    return final
