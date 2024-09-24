f4e303e2ad0820084b3f51d9efa2a42fee4509b7
MuhammadYousufMateen9 <167859198+MuhammadYousufMateen9@users.noreply.github.com>
2024-09-24 18:56:53 +0500

    Update and rename search.py to index.py

---
+++ index.py
@@ -0,0 +1,5 @@
+def index(lst,data):
+    for i in range(len(lst)):
+        if lst[i] == data:
+            return i
+    return None
--- search.py
+++
@@ -1,6 +0,0 @@
-class methodMania:    
-    def search(lst,data):
-            for i in range(len(lst)):
-                if lst[i] == data:
-                    return i
-            return None
