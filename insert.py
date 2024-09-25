8d4905feacd39bc17705ea7055003b57b47ef720
MuhammadYousufMateen9 <167859198+MuhammadYousufMateen9@users.noreply.github.com>
2024-09-25 18:30:00 +0500

    Update insert.py

--- insert.py
+++ insert.py
@@ -1,7 +1,19 @@
-def insert(lst,data,pos):
+def ins(lst,data,pos):
+
+    #changing datatype from int to list
     data=[data]
+
+    #slicing original list from 0 to the position given
     list1=lst[:pos]
+
+    #slicing original list from position given to last 
     list2=lst[pos:]
+
+    #concatenating list 1 and data to be inserted
     list1=list1+data
+
+    #concatenating list 1 and list 2 
     final=list1+list2
+
+    #return final list after insertion
     return final
